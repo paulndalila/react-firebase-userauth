de232fa1d7f5553c9a4f7b8d6e699f2b080344c4
Paul Ndalila <104162113+paulndalila@users.noreply.github.com>
2023-06-22 15:06:59 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,27 @@ import './App.css';
 import React from 'react';
 import Login from './Login';
 import Home from './Home';
-import Welcome from './WelcomePage';
 import Register from './Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from './WelcomePage';
 
 function App() {
 
   return (
 
-    <BrowserRouter>      
+    <BrowserRouter>   
 
       <Routes>
-          <Route path="/home" element = { <Home/> } />
-          <Route path='/register' element = { <Register/> } />
-          <Route index element={ <Welcome/> } />
-          <Route path="/login" element = { <Login/> } />
+
+        <Route path="/home" element = { <Home/> } />
+        <Route path='/register' element = { <Register/> } />
+        <Route index element={ <Welcome/> } />
+        <Route path="/login" element = { <Login/> } />
+
       </Routes>
-    
+
     </BrowserRouter>
-
-
+    
   );
 }
 
